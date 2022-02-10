8c4ac4b71111df7ff9033977a7f2a2a30b50db48
Eduard-WA <eduard.galstyan@willing-able.dev>
2022-02-10 03:24:24 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-t1ulma-gte1ip
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-t1ulma-gte1ip)
---
+++ src/app/product-alert/product-alert.component.ts
@@ -0,0 +1,13 @@
+import { Component, Output, Input , EventEmitter} from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alert',
+  templateUrl: './product-alert.component.html',
+  styleUrls: ['./product-alert.component.css']
+})
+export class ProductAlertComponent {
+  @Input() product!: Product | undefined;
+  @Output() notify = new EventEmitter();
+  
+}
